13a3cf16d53d162ad8dce82f0d4ffccfded9175a
ripon4521 <riponakondo4521@gmail.com>
2025-06-25 00:04:50 +0600

    updated

--- config/index.js
+++ config/index.js
@@ -2,4 +2,4 @@ const dev = process.env.NODE_ENV !== "production";
 
 export const server = dev 
     ? "http://localhost:3000" 
-    : "https://amar-kena-kata-font-end.vercel.app/";
+    : "https://amarkenakatasupershop.com/"
